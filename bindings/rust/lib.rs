e5efe5e7080e2ce021f591f5fc62e41f90d40959
bgevko <bgevko@gmail.com>
2026-06-17 07:16:36 -0500

    feat(rust): expose modern tree-sitter binding

--- bindings/rust/lib.rs
+++ bindings/rust/lib.rs
@@ -1,52 +1,61 @@
 //! This crate provides ca65 language support for the [tree-sitter][] parsing library.
 //!
-//! Typically, you will use the [language][language func] function to add this language to a
-//! tree-sitter [Parser][], and then use the parser to parse some code:
+//! Typically, you will use the [`LANGUAGE`][] constant to add this language to a
+//! tree-sitter [`Parser`][], and then use the parser to parse some code:
 //!
 //! ```
-//! let code = "";
+//! let code = ".segment \"CODE\"\nreset:\n    lda #$00\n";
 //! let mut parser = tree_sitter::Parser::new();
-//! parser.set_language(tree_sitter_ca65::language()).expect("Error loading ca65 grammar");
+//! parser
+//!     .set_language(&tree_sitter_ca65::LANGUAGE.into())
+//!     .expect("Error loading ca65 grammar");
 //! let tree = parser.parse(code, None).unwrap();
+//! assert!(!tree.root_node().has_error());
 //! ```
 //!
-//! [Language]: https://docs.rs/tree-sitter/*/tree_sitter/struct.Language.html
-//! [language func]: fn.language.html
-//! [Parser]: https://docs.rs/tree-sitter/*/tree_sitter/struct.Parser.html
+//! [`Parser`]: https://docs.rs/tree-sitter/*/tree_sitter/struct.Parser.html
 //! [tree-sitter]: https://tree-sitter.github.io/
 
-use tree_sitter::Language;
+use tree_sitter_language::LanguageFn;
 
-extern "C" {
-    fn tree_sitter_ca65() -> Language;
+unsafe extern "C" {
+    fn tree_sitter_ca65() -> *const ();
 }
 
-/// Get the tree-sitter [Language][] for this grammar.
+/// The tree-sitter [`LanguageFn`][LanguageFn] for this grammar.
 ///
-/// [Language]: https://docs.rs/tree-sitter/*/tree_sitter/struct.Language.html
-pub fn language() -> Language {
-    unsafe { tree_sitter_ca65() }
-}
+/// [LanguageFn]: https://docs.rs/tree-sitter-language/*/tree_sitter_language/struct.LanguageFn.html
+pub const LANGUAGE: LanguageFn = unsafe { LanguageFn::from_raw(tree_sitter_ca65) };
 
 /// The content of the [`node-types.json`][] file for this grammar.
 ///
 /// [`node-types.json`]: https://tree-sitter.github.io/tree-sitter/using-parsers#static-node-types
-pub const NODE_TYPES: &'static str = include_str!("../../src/node-types.json");
+pub const NODE_TYPES: &str = include_str!("../../src/node-types.json");
 
-// Uncomment these to include any queries that this grammar contains
-
-// pub const HIGHLIGHTS_QUERY: &'static str = include_str!("../../queries/highlights.scm");
-// pub const INJECTIONS_QUERY: &'static str = include_str!("../../queries/injections.scm");
-// pub const LOCALS_QUERY: &'static str = include_str!("../../queries/locals.scm");
-// pub const TAGS_QUERY: &'static str = include_str!("../../queries/tags.scm");
+/// The default highlights query for this grammar.
+pub const HIGHLIGHTS_QUERY: &str = include_str!("../../queries/highlights.scm");
 
 #[cfg(test)]
 mod tests {
     #[test]
-    fn test_can_load_grammar() {
+    fn can_load_grammar() {
         let mut parser = tree_sitter::Parser::new();
         parser
-            .set_language(super::language())
+            .set_language(&super::LANGUAGE.into())
             .expect("Error loading ca65 language");
+    }
+
+    #[test]
+    fn parses_basic_ca65() {
+        let mut parser = tree_sitter::Parser::new();
+        parser
+            .set_language(&super::LANGUAGE.into())
+            .expect("Error loading ca65 language");
+
+        let tree = parser
+            .parse(".segment \"CODE\"\nreset:\n    lda #$00\n", None)
+            .expect("parser should return a tree");
+
+        assert!(!tree.root_node().has_error());
     }
 }
